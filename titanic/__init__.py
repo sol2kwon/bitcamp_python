46d352baeae86722f8f04192728feec4d9aedd9f
sol2kwon <amorfatisole@gmail.com>
2022-03-16 22:47:29 +0900

    2022-03-16

--- titanic/__init__.py
+++ titanic/__init__.py
@@ -4,12 +4,13 @@ from titanic.models import TitanicModel
 from titanic.template import TitanicTemplate
 if __name__ == '__main__':
     view = TitanicView()
-    model = TitanicModel(train_fname='train.csv',test_fname = 'test.csv')
     while 1:
         menu = input('1.템플릿 2.전처리')
         if menu =='1':
          print('### 1.템플릿 ###')
-         template = TitanicTemplate('fname = train.csv')
+         template = TitanicTemplate(fname = 'train.csv')
+         template.visualize()
+         template.draw_survived()
          break
         elif menu=='2':
          print('### 2.전처리 ###')
--- titanic/models.py
+++ titanic/models.py
@@ -9,8 +9,8 @@ class TitanicModel(object):
     def __init__(self,train_fname,test_fname):
             self.train = self.model.new_model(train_fname)
             self.test = self.model.new_model(test_fname)
-            ic(f'트레인 컬럼 {self.df.columns}')
-            ic(f'트레인 헤드 {self.df.head()}')
+            ic(f'트레인 컬럼 {self.train.columns}')
+            ic(f'트레인 헤드 {self.train.head()}')
             # id 추출
 
     def preprocess(self):
