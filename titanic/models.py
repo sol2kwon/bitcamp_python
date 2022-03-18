42a5141048741237cb67d22344d63623ff3cfcff
sol2kwon <amorfatisole@gmail.com>
2022-03-18 17:55:06 +0900

    2022-03-18

--- titanic/models.py
+++ titanic/models.py
@@ -1,18 +1,21 @@
+import pandas as pd
 from icecream import ic
 
+import titanic
 from context.domains import Dataset
 from context.models import Model
+import numpy as np
 
 
 class TitanicModel(object):
-
     model = Model()
     dataset = Dataset()
 
     def preprocess(self, train_fname, test_fname):
         this = self.dataset
         that = self.model
-        feature = ['PassengerId','Survived','Pclass','Name','Sex','Age','SibSp','Parch','Ticket','Fare','Cabin','Embarked']
+        feature = ['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare',
+                   'Cabin', 'Embarked']
         # 데이터셋은 Train, Test, Validation 3종류로 나뉜다
         this.train = that.new_dframe(train_fname)
         this.test = that.new_dframe(test_fname)
@@ -20,17 +23,19 @@ class TitanicModel(object):
         this.label = this.train['Survived']
         this.train = this.train.drop('Survived', axis=1)
         # Entity 에서 Object 로 전환
-        this = self.drop_feature(this, 'SibSp','Parch','Ticket','Cabin')
+        this = self.drop_feature(this, 'SibSp', 'Parch', 'Ticket', 'Cabin')
         # self.kwargs_sample(name='이순신') kwargs 샘플... 타이타닉 흐름과 무관
         this = self.extract_title_from_name(this)
-        self.remove_duplicate(this)
-        # this = self.title_nominal(this)
-
-        # this = self.name_nominal(this)
-        '''
+        title_mapping = self.remove_duplicate(this)
+        this = self.title_nominal(this, title_mapping)
+        this = self.drop_feature(this, 'Name')
         this = self.sex_nominal(this)
-        this = self.age_ratio(this)
+        this = self.drop_feature(this, 'Sex')
         this = self.embarked_nominal(this)
+        '''
+
+        this = self.age_ratio(this)
+
         this = self.pclass_ordinal(this)
         this = self.fare_ratio(this)
         '''
@@ -40,7 +45,9 @@ class TitanicModel(object):
 
     @staticmethod
     def df_info(this):
-        [ic(f'{i.info()}') for i in [this.train, this.test]]
+        [print(f'{i.info()}') for i in [this.train, this.test]]
+        ic(this.train.head(3))
+        ic(this.test.head(3))
 
     @staticmethod
     def null_check(this):
@@ -48,12 +55,12 @@ class TitanicModel(object):
 
     @staticmethod
     def id_info(this):
-        ic(f'9. id 의 타입  {type(this.id)}')
-        ic(f'10. id 의 상위 3개 {this.id[:3]}')
+        ic(f'id 의 타입  {type(this.id)}')
+        ic(f'id 의 상위 3개 {this.id[:3]}')
 
     @staticmethod
     def drop_feature(this, *feature) -> object:
-        ic(type(feature)) # ic| type(feature): <class 'tuple'>
+        # ic(type(feature)) # ic| type(feature): <class 'tuple'>
         '''
         for i in [this.train, this.test]:
             for j in feature:
@@ -63,8 +70,8 @@ class TitanicModel(object):
 
     @staticmethod
     def kwargs_sample(**kwargs) -> None:
-        ic(type(kwargs))
-        {print(''.join(f'key:{i}, val:{j}')) for i, j in kwargs.items()} # key:name, val:이순신
+        # ic(type(kwargs))
+        {print(''.join(f'key:{i}, val:{j}')) for i, j in kwargs.items()}  # key:name, val:이순신
 
     '''
     Categorical vs. Quantitative
@@ -73,24 +80,23 @@ class TitanicModel(object):
     '''
 
     @staticmethod
-    def pclass_ordinal(this)->object:
+    def pclass_ordinal(this) -> object:
         return this
 
     @staticmethod
     def extract_title_from_name(this) -> None:
-        combine = [this.train, this.test]
-        for dataset in combine:
-            dataset['Title'] = dataset.Name.str.extract('([A-Za-z]+)\.', expand=False)
-        ic(this.train.head(5))
+        for these in [this.train, this.test]:
+            these['Title'] = these.Name.str.extract('([A-Za-z]+)\.', expand=False)
+        # ic(this.train.head(5))
         return this
 
     @staticmethod
     def remove_duplicate(this) -> None:
         a = []
-        for dataset in [this.train, this.test]:
-            a += list(set(dataset['Title']))
+        for these in [this.train, this.test]:
+            a += list(set(these['Title']))
         a = list(set(a))
-        print(f'>>> {a}')
+        # print(f'>>> {a}')
         '''
         ['Mr', 'Sir', 'Major', 'Don', 'Rev', 'Countess', 'Lady', 'Jonkheer', 'Dr',
         'Miss', 'Col', 'Ms', 'Dona', 'Mlle', 'Mme', 'Mrs', 'Master', 'Capt']
@@ -101,31 +107,52 @@ class TitanicModel(object):
         Master
         Mrs
         '''
-        title_mapping = {'Mr': 1, 'Miss': 2, 'Mrs':3, 'Master':4, 'Royal':5, 'Rare': 6}
+        title_mapping = {'Mr': 1, 'Ms': 2, 'Mrs': 3, 'Master': 4, 'Royal': 5, 'Rare': 6}
         return title_mapping
 
     @staticmethod
-    def title_nominal(this,title_mapping) -> object:
-        combine = [this.train, this.test]
-        for dataset in combine:
-            dataset['Title'] = dataset['Title'].replace(['Countess', 'Lady', 'Sir'],' Royal')
-            dataset['Title'] = dataset['Title'].replace(['Capt','Col','Don','Dr','Major','Rev','Jonkheer','Dona','Mme'],'Rare')
-            dataset['Title'] = dataset['Title'].replace(['Countess', 'Lady', 'Sir'], ' Royal')
-            dataset['Title'] = dataset['Title'].replace(['Countess', 'Lady', 'Sir'], ' Royal')
-
-            ic(dataset['Title'])
+    def title_nominal(this, title_mapping) -> object:
+        for these in [this.train, this.test]:
+            these['Title'] = these['Title'].replace(['Countess', 'Lady', 'Sir'], 'Royal')
+            these['Title'] = these['Title'].replace(
+                ['Capt', 'Col', 'Don', 'Dr', 'Major', 'Rev', 'Jonkheer', 'Dona', 'Mme'], 'Rare')
+            these['Title'] = these['Title'].replace(['Mlle'], 'Mr')
+            these['Title'] = these['Title'].replace(['Miss'], 'Ms')
+            # Master 는 변화없음
+            # Mrs 는 변화없음
+            these['Title'] = these['Title'].fillna(0)
+            these['Title'] = these['Title'].map(title_mapping)
         return this
 
     @staticmethod
     def age_ratio(this) -> object:
+        train = this.train
+        test = this.test
+        age_mapping = {'Unknown': 0, 'Baby': 1, 'Child': 2, 'Teenager': 3, 'Student': 4,
+                       'Young Adult': 5, 'Adult': 6, 'Senior': 7}
+        train['Age'] = train['Age'].fillna(-0.5)
+        test['Age'] = test['Age'].fillna(-0.5)
+        bins = [-1, 0, 5, 12, 18, 24, 35, 60, np.inf]
+        labels = ['Unknown', 'Baby', 'Child', 'Teenager', 'Student', 'Young Adult', 'Adult', 'Senior']
+        for these in train, test:
+            # pd.cut() 을 사용하시오. 다른 곳은 고치지 말고 다음 두 줄만 코딩하시오
+            these['AgeGroup'] = pd.cut(titanic['Age'],3, labels=['child','young','old'])  # pd.cut() 을 사용
+            these['AgeGroup'] = map(['child','young','old'])  # map() 을 사용
         return this
 
     @staticmethod
-    def sex_nominal(this)->object:
+    def sex_nominal(this) -> object:
+        gender_mapping = {'male': 0, 'female': 1}
+        for these in [this.train, this.test]:
+            these['Gender'] = these['Sex'].map(gender_mapping)
         return this
 
     @staticmethod
     def embarked_nominal(this) -> object:
+        embarked_mapping = {'S': 1, 'C': 2, 'Q': 3}
+        this.train = this.train.fillna({'Embarked': 'S'})
+        for these in [this.train, this.test]:
+            these['Embarked'] = these['Embarked'].map(embarked_mapping)
         return this
 
     @staticmethod
